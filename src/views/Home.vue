b0e221805cac9d18e9952003c5b49c76199b1375
ilyas <emailpribadiilyas@gmail.com>
2020-10-19 21:31:04 +0700

    experiment with pulsating map and flood data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,15 +8,34 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      floodData: [
+        {
+          title: "Cimahi",
+          latitude: -6.87222,
+          longitude: 107.5425,
+        },
+        {
+          title: "Duren Sawit",
+          latitude: -6.229541,
+          longitude: 106.918153,
+          tweetNum: 10,
+          confidence: 10
+        },
+      ]
+    }
+  },
   mounted() {
     const {
       am4core,
       am4themes_animated,
       am4maps,
-      am4geodata_worldLow
+      // am4geodata_worldLow,
+      am4geodata_indonesiaLow
     } = window;
 
-    am4core.ready(function() {
+    am4core.ready(() => {
       // Themes begin
       am4core.useTheme(am4themes_animated);
       // Themes end
@@ -25,7 +44,8 @@ export default {
       var chart = am4core.create("chartdiv", am4maps.MapChart);
 
       // Set map definition
-      chart.geodata = am4geodata_worldLow;
+      // chart.geodata = am4geodata_worldLow;
+      chart.geodata = am4geodata_indonesiaLow;
 
       // Set projection
       chart.projection = new am4maps.projections.Miller();
@@ -81,120 +101,12 @@ export default {
         });
       }
 
-      var colorSet = new am4core.ColorSet();
+      const colorSet = new am4core.ColorSet();
 
-      imageSeries.data = [
-        {
-          title: "Brussels",
-          latitude: 50.8371,
-          longitude: 4.3676,
-          color: colorSet.next()
-        },
-        {
-          title: "Copenhagen",
-          latitude: 55.6763,
-          longitude: 12.5681,
-          color: colorSet.next()
-        },
-        {
-          title: "Paris",
-          latitude: 48.8567,
-          longitude: 2.351,
-          color: colorSet.next()
-        },
-        {
-          title: "Reykjavik",
-          latitude: 64.1353,
-          longitude: -21.8952,
-          color: colorSet.next()
-        },
-        {
-          title: "Moscow",
-          latitude: 55.7558,
-          longitude: 37.6176,
-          color: colorSet.next()
-        },
-        {
-          title: "Madrid",
-          latitude: 40.4167,
-          longitude: -3.7033,
-          color: colorSet.next()
-        },
-        {
-          title: "London",
-          latitude: 51.5002,
-          longitude: -0.1262,
-          url: "http://www.google.co.uk",
-          color: colorSet.next()
-        },
-        {
-          title: "Peking",
-          latitude: 39.9056,
-          longitude: 116.3958,
-          color: colorSet.next()
-        },
-        {
-          title: "New Delhi",
-          latitude: 28.6353,
-          longitude: 77.225,
-          color: colorSet.next()
-        },
-        {
-          title: "Tokyo",
-          latitude: 35.6785,
-          longitude: 139.6823,
-          url: "http://www.google.co.jp",
-          color: colorSet.next()
-        },
-        {
-          title: "Ankara",
-          latitude: 39.9439,
-          longitude: 32.856,
-          color: colorSet.next()
-        },
-        {
-          title: "Buenos Aires",
-          latitude: -34.6118,
-          longitude: -58.4173,
-          color: colorSet.next()
-        },
-        {
-          title: "Brasilia",
-          latitude: -15.7801,
-          longitude: -47.9292,
-          color: colorSet.next()
-        },
-        {
-          title: "Ottawa",
-          latitude: 45.4235,
-          longitude: -75.6979,
-          color: colorSet.next()
-        },
-        {
-          title: "Washington",
-          latitude: 38.8921,
-          longitude: -77.0241,
-          color: colorSet.next()
-        },
-        {
-          title: "Kinshasa",
-          latitude: -4.3369,
-          longitude: 15.3271,
-          color: colorSet.next()
-        },
-        {
-          title: "Cairo",
-          latitude: 30.0571,
-          longitude: 31.2272,
-          color: colorSet.next()
-        },
-        {
-          title: "Pretoria",
-          latitude: -25.7463,
-          longitude: 28.1876,
-          color: colorSet.next()
-        }
-      ];
+      imageSeries.data = this.floodData.map(item => ({
+        ...item,
+        color: colorSet.next()
+      }))
     }); // end am4core.ready()
   }
 };
