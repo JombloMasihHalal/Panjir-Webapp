cd50a84185bca63878de178c23e00d82c281dc91
ilyas <emailpribadiilyas@gmail.com>
2020-10-21 21:10:27 +0700

    add tweet list and tweet item

---
+++ src/views/Home/components/TweetItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tweet-item">
+    tweet item
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TweetItem'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home/components/TweetList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tweet-list">
+    tweet
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TweetList',
+}
+</script>
+
+<style>
+
+</style>
